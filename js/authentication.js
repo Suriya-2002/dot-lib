297512aa3bb387d729a8d143dcc12b64fbac278f
Suriya KS <76834126+Suriya-2002@users.noreply.github.com>
2022-05-06 13:32:06 +0530

    Implemented authentication functionality

--- js/authentication.js
+++ js/authentication.js
@@ -1,3 +1,45 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js';
+import {
+    getAuth,
+    onAuthStateChanged,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+} from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js';
+import {
+    getFirestore,
+    collection,
+    onSnapshot,
+    doc,
+    getDocs,
+    getDoc,
+    setDoc,
+    updateDoc,
+    query,
+    where,
+    limit,
+} from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyC0Knk7W5pfY4eRlM0vNMbYznV47fBHAuY',
+    authDomain: 'dot-lib.firebaseapp.com',
+    projectId: 'dot-lib',
+    storageBucket: 'dot-lib.appspot.com',
+    messagingSenderId: '811914504552',
+    appId: '1:811914504552:web:87399527dc7c99d52a8edd',
+    measurementId: 'G-D438YFH6TJ',
+};
+
+const state = {
+    userID: '',
+    name: '',
+    email: '',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
 const authenticationContainer = document.querySelector('.authentication');
 
 const formSignIn = document.querySelector('.authentication__form--sign-in');
@@ -14,6 +56,96 @@ const errorSignUp = document.querySelector('.authentication__error-message--sign
 const signUpButton = document.querySelector('.button--sign-up');
 const signInOverlayButton = document.querySelector('.button--overlay-sign-in');
 const signUpOverlayButton = document.querySelector('.button--overlay-sign-up');
+
+const authenticateUserSignIn = async credentials => {
+    try {
+        const { email, password } = credentials;
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        state.userID = userCredential.user.uid;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const authenticateUserSignUp = async credentials => {
+    try {
+        const { name, email, password } = credentials;
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+        state.userID = userCredential.user.uid;
+
+        const userDocumentReference = doc(db, 'users', state.userID);
+        await setDoc(userDocumentReference, { name: name, email: email });
+    } catch (error) {
+        throw error;
+    }
+};
+
+const validateUserSignIn = error => {
+    signInButton.classList.remove('button--disabled');
+
+    if (error) {
+        errorSignIn.innerHTML = error.message;
+        return;
+    }
+
+    window.location.href = './../index.html';
+};
+
+const validateUserSignUp = error => {
+    signUpButton.classList.remove('button--disabled');
+
+    if (error) {
+        errorSignUp.innerHTML = error.message;
+        return;
+    }
+
+    window.location.href = './../index.html';
+};
+
+const controlAuthenticationSignIn = async credentials => {
+    try {
+        await authenticateUserSignIn(credentials);
+        validateUserSignIn();
+    } catch (error) {
+        validateUserSignIn(error);
+    }
+};
+
+const controlAuthenticationSignUp = async credentials => {
+    try {
+        await authenticateUserSignUp(credentials);
+        validateUserSignUp();
+    } catch (error) {
+        validateUserSignUp(error);
+    }
+};
+
+formSignIn.addEventListener('submit', event => {
+    event.preventDefault();
+    signInButton.classList.add('button--disabled');
+
+    const credentials = {
+        email: emailSignIn.value,
+        password: passwordSignIn.value,
+    };
+
+    controlAuthenticationSignIn(credentials);
+});
+
+formSignUp.addEventListener('submit', event => {
+    event.preventDefault();
+    signUpButton.classList.add('button--disabled');
+
+    const credentials = {
+        name: nameSignUp.value,
+        email: emailSignUp.value,
+        password: passwordSignUp.value,
+    };
+
+    controlAuthenticationSignUp(credentials);
+});
 
 signInOverlayButton.addEventListener('click', () => {
     authenticationContainer.classList.remove('authentication--sign-up-active');
